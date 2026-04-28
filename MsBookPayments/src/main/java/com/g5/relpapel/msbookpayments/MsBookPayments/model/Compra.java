d2e41639c9e336b6a7e171727f0e3bbf27c85626
bspoloo <animetx098@gmail.com>
2026-04-28 14:33:27 -0400

    added test for test-compra-controller

--- MsBookPayments/src/main/java/com/g5/relpapel/msbookpayments/MsBookPayments/model/Compra.java
+++ MsBookPayments/src/main/java/com/g5/relpapel/msbookpayments/MsBookPayments/model/Compra.java
@@ -2,12 +2,16 @@ package com.g5.relpapel.msbookpayments.MsBookPayments.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Modelo que mapea de la base de datos usando JPA.
  */
 @Entity
 @Data
+@Getter
+@Setter
 public class Compra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
